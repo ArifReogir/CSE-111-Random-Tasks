e87b135550a5638a9a8085aea7182a09d4242bb4
Arif Reogir <165476513+ArifReogir@users.noreply.github.com>
2025-12-23 00:30:33 +0600

    first commit

---
+++ FruitTester.java
@@ -0,0 +1,63 @@
+/*Write the Mango and the Jackfruit classes derived from 
+Fruit class so that the following code generates the output below:
+ */
+
+public class FruitTester { // Driver code
+    public static void testFruit(Fruit f) {
+        System.out.println("----Printing Detail-----");
+        if (f.hasFormalin()) {
+            System.out.println("Do not eat the " + f.getName() + ".");
+            System.out.println(f);
+        } else {
+            System.out.println("Eat the " + f.getName() + ".");
+            System.out.println(f);
+        }
+    }
+
+    public static void main(String[] args) {
+        Mango m = new Mango();
+        testFruit(m);
+        Jackfruit j = new Jackfruit();
+        testFruit(j);
+    }
+}
+
+// Class starts here
+
+class Fruit { // Parent class
+    private boolean formalin = false;
+    private String name = "";
+
+    public Fruit(boolean formalin, String name) {
+        this.formalin = formalin;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean hasFormalin() {
+        return formalin;
+    }
+}
+
+class Mango extends Fruit {
+    public Mango() {
+        super(true, "Mango");
+    }
+
+    public String toString() {
+        return "Mangos are bad for you";
+    }
+}
+
+class Jackfruit extends Fruit {
+    public Jackfruit() {
+        super(false, "Jackfruit");
+    }
+
+    public String toString() {
+        return "Jackfruits are good for you";
+    }
+}
